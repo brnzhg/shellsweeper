ca7f1aca8a03790c1a27ae41d8507b3a41d9a1c7
Brian Zhang <brnzhg@gmail.com>
2018-11-25 00:33:29 -0600

    about to majorly move things around, extend Board

--- src/ChooseFinite.hs
+++ src/ChooseFinite.hs
@@ -9,8 +9,10 @@ ScopedTypeVariables
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 
 module ChooseFinite (
-chooseAndSwapIndicesToK
-, indexPermutation
+RFinite(..)
+, swapIndexPairs
+, indexPairsChooseK
+, indexPairsPermuteAll
 ) where
 
 import System.Random (Random(..))
@@ -27,7 +29,7 @@ import qualified Data.Vector.Generic.Mutable.Base as V
 import qualified Data.Vector.Generic.Mutable.Sized as VS
 
 
-newtype RFinite a = RFinite { getFinite :: Finite a }
+newtype RFinite a = RFinite { getFin :: Finite a }
   deriving (Eq, Ord, Num, Real, Integral, Enum, Bounded)
 
 instance (KnownNat n) => Random (RFinite n) where
@@ -38,76 +40,28 @@ instance (KnownNat n) => Random (RFinite n) where
   random = randomR (minBound, maxBound)
 
 
-chooseAndSwapIndicesToK :: forall v n m m'.
-  (MonadInterleave m
-  , PrimMonad m'
+swapIndexPairs :: forall v n m a.
+  (PrimMonad m
   , KnownNat n
-  , V.MVector v (Finite n)) =>
-  Finite (n + 1) -> m (m' (VS.MVector v n (PrimState m') (Finite n)))
-chooseAndSwapIndicesToK k = interleave $ do
-  (indexSwapPairs :: [(RFinite n, RFinite n)]) <-
-    sequence
-    . take (fromIntegral k)
-    . map (\i -> (,) i <$> getRandomR (i, maxBound))
-    $ enumFrom minBound
-  return $ do
-    (v :: VS.MVector v n (PrimState m') (Finite n)) <- VS.new
-    forM_ (enumFrom minBound) (\i -> VS.write v i i)
-    forM_ indexSwapPairs (\(RFinite i1, RFinite i2) -> VS.swap v i1 i2)
-    return v
+  , V.MVector v a) =>
+  [(Finite n, Finite n)]
+  -> VS.MVector v n (PrimState m) a
+  -> m ()
+swapIndexPairs = (. uncurry . VS.swap) . forM_
 
-indexPermutation :: forall v n m m'.
+indexPairsChooseK :: forall n m.
   (MonadInterleave m
-  , PrimMonad m'
-  , KnownNat n
-  , V.MVector v (Finite n)) => m (m' (VS.MVector v n (PrimState m') (Finite n)))
-indexPermutation = chooseAndSwapIndicesToK (maxBound :: Finite (n + 1))
-
-
-{-
-randSwapRight :: forall v n m m' a.
-  (MonadRandom m
-  , PrimMonad m'
-  , V.MVector v a
   , KnownNat n) =>
-  VS.MVector v n (PrimState m') a -> Finite n -> m (m' (Finite n))
-randSwapRight v i = do
-  j <- getRandomR (i, maxBound)
-  return $ do
-    VS.swap v i j
-    return j
-
--- choose random permutation of K from vector,
--- swap them into the first K elements of the vector (indices 0 .. K-1)
-shuffleFirstK :: forall v n m m' a.
-  (MonadRandom m
-  , PrimMonad m'
-  , V.MVector v a
-  , KnownNat n) =>
-  VS.MVector v n (PrimState m') a -> Finite (n + 1) -> m (m' ())
-shuffleFirstK v k = case unshift k of
-  Nothing -> return $ return ()
-  Just k' -> fmap sequence_ $ forM [minBound..k'] $ randSwapRight v
-
-shuffle :: forall v n m m' a.
-  (MonadRandom m
-  , PrimMonad m'
-  , V.MVector v a
-  , KnownNat n) =>
-  VS.MVector v n (PrimState m') a -> m (m' ())
-shuffle = flip shuffleFirstK maxBound
-
-
-randChooseGridCoordIndices :: forall g n. (RandomGen g, KnownNat n) =>
-  g -> F.Finite (n + 1) -> [F.Finite n]
-randChooseGridCoordIndices gen k = runST st
+  Finite (n + 1) -> m [(Finite n, Finite n)]
+indexPairsChooseK k = sequence
+  . take (fromIntegral k)
+  . map indexToSwapPair
+  $ enumFrom minBound
   where
-    st :: (forall s. ST s [F.Finite n])
-    st = do
-      (v :: VGMS.MVector VM.MVector n s (F.Finite n)) <- VGMS.new
-      forM_ (enumFrom minBound) (\i -> VGMS.write v i i) --TODO make better with lenses
-      evalRand (shuffleFirstK v k) gen
-      (vf :: VGS.Vector V.Vector n (F.Finite n)) <- VGS.freeze v
-      return $ take (fromIntegral k) $ toList vf
+    indexToSwapPair ri@(RFinite i) =
+      ((,) i . getFin) <$> (getRandomR (ri, maxBound))
 
--}
+indexPairsPermuteAll :: forall n m.
+  (MonadInterleave m
+  , KnownNat n) => m [(Finite n, Finite n)]
+indexPairsPermuteAll = indexPairsChooseK (maxBound :: Finite (n + 1))
--- src/Game.hs
+++ src/Game.hs
@@ -10,18 +10,78 @@ ScopedTypeVariables
 
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 
+--or gamestate? all nonui aspects of game state transformations
+--where cursor is doesnt matter for example
 module Game() where
 
 import Data.Foldable --
 import Data.Traversable --
 
+import Control.Monad.ST (ST, runST)
 import Control.Monad.State.Lazy (StateT(..), get, put, lift, evalStateT)
+import Control.Monad.Random (MonadInterleave)
 
-data BoardTileGST = BoardTileGST
-  { isRevealed :: Bool
+import Data.Finite
+import GHC.TypeLits
+
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Sized as VS
+import qualified Data.Vector.Generic.Mutable.Base as VGM
+import qualified Data.Vector.Generic.Mutable.Sized as VGMS
+
+import ChooseFinite (swapIndexPairs, indexPairsChooseK)
+import Grid (Grid(..), GridCoord(..), GridIndex(..), gridIndexCoord)
+import Board (BoardTile(..), makeTileVector)
+
+
+data BoardTileState = BoardTileState
+  { tile :: BoardTile
+  , isRevealed :: Bool
   , isMarked :: Bool
   }
+--this could be thing in State Monad
+--add numAdjMineMarked
 
---data BoardGST
+data BoardState (n :: Nat) (n' :: Nat) = BoardState
+  { tileGrid :: Grid n n' BoardTileState
+  , numRevealed :: Finite (n * n' + 1)
+  , numMarked :: Finite (n * n' + 1)
+  }
+
+data GameEndState = WinState | LoseState
+
+data GameState (n :: Nat) (n' :: Nat) = GameState
+  { board :: BoardState n n'
+  , gameEnd :: Maybe GameEndState
+  }
+--gamestate has score? parameterize it later?
+--has win or lose or something (maybe bool iswin)
 
 
+--reset (dont regenerate)
+--generate start
+--reveal tile
+--mark tile
+
+
+--exchangeAndSwap :: forall v n m a. (PrimMonad m )
+
+--TODO move this to board
+--board needs to change from taking mine indices
+--(replacement index, mine index)
+{-
+mineVectorFromIndexPairs :: forall v n m.
+  (MonadInterleave m
+  , KnownNat n) =>
+  Finite (n + 1)
+  -> [(Finite n, Finite n)]
+  -> VS.Vector n Bool
+mineVectorFromIndexPairs numMines indexPairs =
+  runST st
+  where
+    st :: (forall s. ST s (VS.Vector n Bool))
+    st = do
+      let (v :: VS.MVector VM.MVector n s Bool) =
+            VS.generate (\i -> (shift i) <= numMInes)
+      swapIndexPairs indexPairs v
+-}
