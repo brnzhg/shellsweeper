deee1c2e92d28da34ae9e5a0e8e4f3bc9c17e3d8
Brian Zhang <brnzhg@gmail.com>
2018-11-30 01:20:59 -0600

    refactored gamestate with some lenses, remove randomMineVec and randomTileVec?

--- src/Game.hs
+++ src/Game.hs
@@ -17,6 +17,7 @@ module Game() where
 
 import Data.Foldable --
 import Data.Traversable --
+import Data.Bifunctor (bimap)
 
 import Control.Monad.State.Lazy (StateT(..), get, put, lift, evalStateT)
 import Control.Monad.Random (MonadInterleave)
@@ -24,14 +25,17 @@ import Control.Monad.Random (MonadInterleave)
 import Data.Finite
 import GHC.TypeLits
 
+import Control.Lens ((^.), (%~))
 import qualified Control.Lens as L
 import qualified Control.Lens.Iso as LI
-
+import qualified Control.Lens.Traversal as LT
+  
 import qualified Data.Vector.Mutable as VM
 import qualified Data.Vector.Sized as VS
 import qualified Data.Vector.Generic.Mutable.Base as VGM
 import qualified Data.Vector.Generic.Mutable.Sized as VGMS
 
+import ChooseFinite (indexPairsChooseK)
 import Grid (Grid(..), GridCoord(..), GridIndex(..), gridIndexCoord)
 import Board (BoardTile(..)
               , tileVectorFromMines
@@ -63,8 +67,8 @@ data GameState (n :: Nat) (n' :: Nat) = GameState
 
 data GameEnv (n :: Nat) (n' :: Nat) = GameEnv
   { _numMines :: Finite (n * n' + 1)
-  , _getAdj :: Finite (n * n') -> [Finite (n * n')]
-  --, _getAdj :: GridCoord n n' -> [GridCoord n n']
+  --, _getAdj :: Finite (n * n') -> [Finite (n * n')]
+  , _getAdj :: GridCoord n n' -> [GridCoord n n']
   }
 
 --gamestate has score? parameterize it later?
@@ -97,19 +101,30 @@ startGameStateFromTileGrid g =
                  , _numMarked = 0
                  }
 
---TODO another version where take explicit pairs
---dont do mine vector since mines set by env
+
+startGameStateFromCoordPairs :: forall n n'.
+  (KnownNat n, KnownNat n') =>
+  GameEnv n n'
+  -> [(GridCoord n n', GridCoord n n')]
+  -> GameState n n'
+startGameStateFromCoordPairs gameEnv =
+  startGameStateFromTileGrid
+  . tileVectorFromIndexPairs'
+  . L.over (L.mapped . L.both) (L.view $ LI.from gridIndexCoord)
+  where
+    tileVectorFromIndexPairs' = tileVectorFromMines getAdj'
+                                . mineVectorFromIndexPairs'
+    getAdj' = LT.traverseOf gridIndexCoord . _getAdj $ gameEnv
+    mineVectorFromIndexPairs' = mineVectorFromIndexPairs
+                                . _numMines
+                                $ gameEnv
+
 --TODO use monadreader herer
 --make gameenv more modular like the font
 randomStartGameState :: forall n n' m.
   (KnownNat n, KnownNat n', MonadInterleave m) =>
   GameEnv n n' -> m (GameState n n')
 randomStartGameState gameEnv =
-  startGameStateFromTileGrid <$> (randomTileGrid :: m (Grid n n' BoardTile))
-  where
-    randomTileGrid :: m (VS.Vector (n * n') BoardTile)
-    randomTileGrid =
-      randomTileVector (_getAdj gameEnv) (_numMines gameEnv)
-      --randomTileVector (LI.under (LI.from gridIndexCoord) _getAdj gameEnv) (_numMines gameEnv)
-
-
+  startGameStateFromCoordPairs gameEnv
+  . L.over (L.mapped . L.both) (L.view gridIndexCoord)
+  <$> (indexPairsChooseK $ _numMines gameEnv)
