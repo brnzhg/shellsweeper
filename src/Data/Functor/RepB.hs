e66ab2a035c74e22744e2a894a506086e8e1916d
Brian Zhang <brnzhg@gmail.com>
2018-12-06 00:23:52 -0600

    make vector an instance of RepB, need to move RandomGameState to from Board to new class

--- src/Data/Functor/RepB.hs
+++ src/Data/Functor/RepB.hs
@@ -1,12 +1,23 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Data.Functor.RepB (
   RepresentableB(..)
 ) where
 
 import Data.Functor.Rep
 
+import GHC.TypeLits
+
+import qualified Data.Vector.Generic.Sized as VGS
+import qualified Data.Vector as V
+import Control.Arrow ((&&&))
+
 class Representable f => RepresentableB f where
   update :: f a -> [(Rep f, a)] -> f a
   update fa = updateOver fa id . map fst
 
   updateOver :: f a -> (a -> a) -> [Rep f] -> f a
-  updateOver fa f = update fa . map (\x -> (x, fa `index` x))
+  updateOver fa f = update fa . map (id &&& index fa)
+
+instance KnownNat n => RepresentableB (VGS.Vector V.Vector n) where
+  update = (VGS.//)
